790c7f78c13c3db9848ba2f3227dec88b846dad0
Patrick Dawkins <pjcdawkins@gmail.com>
2016-07-22 15:13:34 +0100

    Create admin view page

--- src/Entity/InvoiceController.php
+++ src/Entity/InvoiceController.php
@@ -61,4 +61,51 @@ class InvoiceController extends \EntityAPIController {
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  function buildContent($entity, $view_mode = 'full', $langcode = NULL, $content = []) {
+    /** @var Invoice $entity */
+    $info = [];
+
+    $info[t('Invoice ID')] = check_plain($entity->invoice_id);
+    $info[t('Owner')] = [
+      '#theme' => 'username',
+      '#account' => $entity->wrapper()->owner->value(),
+    ];
+    $info[t('Invoice number')] = $entity->hasInvoiceNumber()
+      ? [
+        '#theme' => 'commerce_invoice_number',
+        '#invoice_number' => $entity->getInvoiceNumber(),
+      ]
+      : t('Not yet generated');
+    $info[t('Invoice date')] = check_plain(format_date($entity->invoice_date));
+    $info[t('Status')] = commerce_invoice_statuses()[$entity->invoice_status];
+    $info[t('Order')] = isset($entity->order_id)
+      ? l($entity->wrapper()->order->order_number->value(), 'admin/commerce/orders/' . $entity->order_id)
+      : t('None');
+    $info[t('Order revision')] = isset($entity->order_revision_id)
+      ? check_plain($entity->order_revision_id)
+      : t('None');
+    $pattern = $entity->getNumberPattern();
+    $info[t('Invoice number pattern')] = format_string('@label: <code>@pattern</code>', [
+      '@label' => $pattern->label,
+      '@pattern' => $pattern->pattern
+    ]);
+    $info[t('Created')] = check_plain(format_date($entity->created));
+    $info[t('Last modified')] = check_plain(format_date($entity->changed));
+
+    if (!empty($info)) {
+      $content['info'] = ['#theme' => 'table', '#rows' => []];
+      foreach ($info as $label => $value) {
+        $content['info']['#rows'][] = [
+          ['data' => $label, 'header' => TRUE],
+          is_array($value) ? ['data' => $value] : $value,
+        ];
+      }
+    }
+
+    return parent::buildContent($entity, $view_mode, $langcode, $content);
+  }
+
 }
